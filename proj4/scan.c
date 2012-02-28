e9e504e9499e5fb1b9be045ab75334fa2400612e
Russell <russell@chandra>
2012-02-27 20:15:15 -0800

    removed local variable

--- proj4/scan.c
+++ proj4/scan.c
@@ -5,14 +5,13 @@ int *scan(int *a, int n)
   if (n <= 0)
     return 0;
   int *result = (int*) malloc((n+1)*sizeof(int));
-  int sum = 0;
+  result[0] = a[0];
   result[n] = 0;
   int i;
-  for (i=0; i < n; i++) {
+  for (i=1; i < n; i++) {
     if (a[i] > result[n])
       result[n] = a[i];
-    sum += a[i];
-    result[i] = sum;
+    result[i] = a[i] + result[i-1];
   }
   return result;
 }
--- proj4/scan.s
+++ proj4/scan.s
@@ -1,32 +1,42 @@
+	.file	"scan.c"
 	.section	".text"
 	.align 4
 	.global scan
+	.type	scan, #function
+	.proc	0104
 scan:
-	save	%sp, -112, %sp		! allocate a frame
-	cmp	%i1, 0			! (n <= 0) assuming n is 2nd parameter
-	ble	.LL1			! jump to Label 1 when true
-	mov	0, %o0			! delayed branch execution of return 0
-	sll	%i1, 2, %l0		! shift left logical
+	!#PROLOGUE# 0
+	save	%sp, -112, %sp
+	!#PROLOGUE# 1
+	cmp	%i1, 0
+	ble	.LL1
+	mov	0, %o0
+	sll	%i1, 2, %l0
 	call	malloc, 0
 	add	%l0, 4, %o0
-	mov	0, %o3
+	ld	[%i0], %g1
+	st	%g1, [%o0]
+	mov	1, %o2
+	b	.LL10
 	st	%g0, [%o0+%l0]
-	cmp	%o3, %i1
-	bge	.LL1
-	mov	0, %o2
-.LL2:
-	sll	%o3, 2, %o4
+.LL12:
 	ld	[%i0+%o4], %o5
 	ld	[%o0+%l0], %g1
-	add	%o3, 1, %o3
+	add	%o4, %o0, %o3
 	cmp	%o5, %g1
 	ble	.LL6
-	add	%o2, %o5, %o2
+	add	%o2, 1, %o2
 	st	%o5, [%o0+%l0]
 .LL6:
-	cmp	%o3, %i1
-	bl	.LL2
-	st	%o2, [%o0+%o4]
+	ld	[%o3-4], %g1
+	add	%o5, %g1, %g1
+	st	%g1, [%o0+%o4]
+.LL10:
+	cmp	%o2, %i1
+	bl	.LL12
+	sll	%o2, 2, %o4
 .LL1:
 	ret
 	restore %g0, %o0, %o0
+	.size	scan, .-scan
+	.ident	"GCC: (GNU) 3.4.3 (csl-sol210-3_4-branch+sol_rpath)"
