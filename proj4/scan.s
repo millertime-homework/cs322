770b5ef5074fc039773c5a2cc314ed45f63f6884
Russell <russell@chandra>
2012-02-27 19:28:33 -0800

    started commenting. changed label 12 to 2

--- proj4/scan.s
+++ proj4/scan.s
@@ -2,10 +2,10 @@
 	.align 4
 	.global scan
 scan:
-	save	%sp, -112, %sp
-	cmp	%i1, 0
-	ble	.LL1
-	mov	0, %o0
+	save	%sp, -112, %sp		! allocate a frame
+	cmp	%i1, 0			! (n <= 0) assuming n is 2nd parameter
+	ble	.LL1			! jump to Label 1 when true
+	mov	0, %o0			! delayed branch execution of return 0
 	sll	%i1, 2, %l0		! shift left logical
 	call	malloc, 0
 	add	%l0, 4, %o0
@@ -14,7 +14,7 @@ scan:
 	cmp	%o3, %i1
 	bge	.LL1
 	mov	0, %o2
-.LL12:
+.LL2:
 	sll	%o3, 2, %o4
 	ld	[%i0+%o4], %o5
 	ld	[%o0+%l0], %g1
@@ -25,7 +25,7 @@ scan:
 	st	%o5, [%o0+%l0]
 .LL6:
 	cmp	%o3, %i1
-	bl	.LL12
+	bl	.LL2
 	st	%o2, [%o0+%o4]
 .LL1:
 	ret
